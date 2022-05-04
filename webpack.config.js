36287a2993fb233441f6dfad8f77c1615f082405
Alexey Maistrenko <85947242+Alexey369741@users.noreply.github.com>
2022-05-04 14:19:24 +0300

    Changes

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+    },
+    mode: 'development',
+    devServer: {
+        static: './dist',
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/index.html',
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+    },
+};
